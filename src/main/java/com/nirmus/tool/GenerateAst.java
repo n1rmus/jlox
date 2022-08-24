cffd0c9bdb49c8c3781f5e8fd14eb094791c843c
n1rmus <v.nirmus@gmail.com>
2022-08-24 11:35:59 +0600

    Chapter 10

--- src/main/java/com/nirmus/tool/GenerateAst.java
+++ src/main/java/com/nirmus/tool/GenerateAst.java
@@ -26,9 +26,11 @@ public class GenerateAst {
         defineAst(outputDir, "Stmt", Arrays.asList(
             "Block      : List<Stmt> statements",
             "Expression : Expr expression",
+            "Function   : Token name, List<Token> params, List<Stmt> body",
             "If         : Expr condition, Stmt thenBranch, Stmt elseBranch",
             "Var        : Token name, Expr initializer",
             "Print      : Expr expression",
+            "Return     : Token keyword, Expr value",
             "While      : Expr condition, Stmt body"
         ));
     }
